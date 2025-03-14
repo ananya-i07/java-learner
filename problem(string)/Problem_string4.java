681643e607e41c71764e0d5d04d3bcde9648ae16
ananya-i07 <anachauhan1006@gmail.com>
2025-03-14 21:33:14 +0530

    Update Problem_string4.java

--- problem(string)/Problem_string4.java
+++ problem(string)/Problem_string4.java
@@ -1,6 +1,8 @@
 public class Problem_string4 {
     public static void main(String[] args) {
-        
+     String string="This  is very   basic";
+     System.out.println(string.indexOf("  "));
+     System.out.println(string.indexOf("   ")); 
     }
     
 }
