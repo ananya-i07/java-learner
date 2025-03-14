253bc644ce637700796a9107fb926bc3a87e1dd6
ananya-i07 <anachauhan1006@gmail.com>
2025-03-14 14:39:29 +0530

    Add files via upload

---
+++ practice(level1)/Problem_datatype_conversion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+/*take input grade:
+ * encrypt it by adding 8
+ * and give orignal result:
+ * by decrypt it
+ */
+
+public class ProblemD1 {
+
+public static void main(String[] args) {
+     Scanner s = new Scanner(System.in);
+     System.out.println("enter your grade");
+     char grade=s.next().charAt(0);
+     grade=(char)(grade+8);
+     //encrypt it by adding 8.
+     System.out.println(grade);
+     System.out.println("orignal grade:");
+     grade=(char)(grade-8);
+     System.out.println((char)grade);
+     s.close();
+
+}
+}
